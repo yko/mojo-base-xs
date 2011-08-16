0ccf4bb23118512716b24059dd739f41fe1608f4
yko <ykorshak@gmail.com>
2011-08-16 21:05:06 +0300

    fixed xs tests

--- t/lib/BaseTestXS/Base2.pm
+++ t/lib/BaseTestXS/Base2.pm
@@ -1,6 +1,6 @@
-package BaseTest::Base2;
+package BaseTestXS::Base2;
 use Mojo::BaseXS;
-use base 'BaseTest::Base1';
+use base 'BaseTestXS::Base1';
 
 # "Hey, I asked for ketchup! I'm eatin' salad here!"
 has [qw/ears eyes/] => sub {2};
